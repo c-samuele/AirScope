0ce201f0575c225d7a7c929518f10b5994e5b081
samuele <s.caporale1@campus.uniurb.it>
2025-05-23 16:24:53 +0200

    aggiunto jquery e modificato i selettori con la sintassi $

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,4 +1,4 @@
-
+// Versione GUI
 const version = "v0.0.6";
 const versionElements = document.querySelectorAll(".version-number");
 
@@ -6,6 +6,7 @@ for (const el of versionElements) {
   el.textContent = version;
 }
 
+// Gestione del menu con link attivo
 document.querySelectorAll('#menu-nav .nav-link').forEach(link => {
     link.addEventListener('click', function () {
       document.querySelectorAll('#menu-nav .nav-link').forEach(item => {
@@ -14,18 +15,18 @@ document.querySelectorAll('#menu-nav .nav-link').forEach(link => {
       this.classList.add('active');
     });
   });
-
+// intercetto il click sul link del menu
   document.getElementById("dashboard-btn").addEventListener("click", () => contentGenerate("dashboard"));
   document.getElementById("analitics-btn").addEventListener("click", () => contentGenerate("analitics"));
   document.getElementById("request-btn").addEventListener("click", () => contentGenerate("request"));
 
-
+// funzione per generare il contenuto della pagina servizio
  function contentGenerate(type) {
-    const container = document.getElementById("main-section");
+    const container = $("#main-section");
 
     switch(type) {
       case "dashboard":
-        container.innerHTML = `
+        container.html(`
 <div class="container-fluid">
 <!-- TITOLO -->
         <div class="col-12 mb-3">
@@ -34,55 +35,67 @@ document.querySelectorAll('#menu-nav .nav-link').forEach(link => {
 
 <!-- METRICHE -->
     <div class="container-metrics">
-      
         <section class="d-flex flex-row item-card rounded-3">
           <h3 class="title-metrics">CO</h3>
           <div class="divider-metrics"></div>
-          <i class="metrics-value"> <span class="metrics-udm">[mg/m³]</span></i>
+          <i class="metrics-value"><span id="val-co" class="data-avg"></span> <span class="metrics-udm">[mg/m³]</span></i>
         </section>
         
         <section class="d-flex flex-row item-card rounded-3">
           <h3 class="title-metrics">NO2</h3>
           <div class="divider-metrics"></div>
-          <i class="metrics-value"> <span class="metrics-udm">[μg/m³]</span></i>
+          <i class="metrics-value"><span id="val-no2" class="data-avg"></span> <span class="metrics-udm">[μg/m³]</span></i>
         </section>
       
         <section class="d-flex flex-row item-card rounded-3">
           <h3 class="title-metrics">NOx</h3>
           <div class="divider-metrics"></div>
-          <i class="metrics-value"> <span class="metrics-udm">[μg/m³]</span></i>
+          <i class="metrics-value"><span id="val-nox" style="color:red" class="data-avg"></span> <span class="metrics-udm">[μg/m³]</span></i>
         </section>
       
         <section class="d-flex flex-row item-card rounded-3">
           <h3 class="title-metrics">O3</h3>
           <div class="divider-metrics"></div>
-          <i class="metrics-value"> <span class="metrics-udm">[μg/m³]</span></i>
+          <i class="metrics-value"><span id="val-o3" class="data-avg"></span> <span class="metrics-udm">[μg/m³]</span></i>
         </section>
       
         <section class="d-flex flex-row item-card rounded-3">
           <h3 class="title-metrics">PM10</h3>
           <div class="divider-metrics"></div>
-          <i class="metrics-value"> <span class="metrics-udm">[μg/m³]</span></i>
+          <i class="metrics-value"><span id="val-pm10"  class="data-avg"></span> <span class="metrics-udm">[μg/m³]</span></i>
         </section>
     </div>
-</div>`;
+</div>`);
+// Aggiorno i dati sul client dal server
+fetch('http://localhost:3000/api/values_avg')
+  .then(res => res.json())
+  .then(data => {
+    $('#val-co').text(data.CO);
+    $('#val-no2').text(data.NO2);
+    $('#val-nox').text(data.NOX);  
+    $('#val-o3').text(data.O3);
+    $('#val-pm10').text(data.PM10);
+  })
+  .catch(e => console.error(e));
+
         break;
       case "analitics":
-        container.innerHTML = `
+        container.html(`
           <div class="container-fluid">
             <p>Infographics,avg,table</p>
-          </div>`;
+            <canvas id="chart-database" class="rounded-3"></canvas>
+          </div>`);
         break;
       case "request":
-        container.innerHTML = `
+        container.html(`
           <div class="container-fluid">
 <!-- form -->
               <div class="container-form py-3">
                 <div class="col-sm-12 col-md-8 col-lg-6">
                   <h2>POST</h2>
                   <div class="form-api d-flex flex-column rounded-3 p-3">
-                      <form action="/upload" method="POST" enctype="multipart/form-data">
-                          <input class="form-control mb-3" type="file" name="file" />
+                      <form id="form-send" action="/upload" method="POST" enctype="multipart/form-data">
+                          <input class="form-control mb-3" type="file" name="file" required>
                           <button class="btn btn-outline-primary px-3" type="submit">Carica</button>
                       </form>
                   </div>
@@ -91,7 +104,39 @@ document.querySelectorAll('#menu-nav .nav-link').forEach(link => {
               
 
 <!-- end form -->
-          </div>`;
+          </div>`);
+  // Caricamento del file
+document.getElementById('form-send').addEventListener('submit', async (e) => {
+  e.preventDefault(); // Impedisce il ricaricamento della pagina
+
+  // Crea un oggetto FormData con i dati del form, incluso il file
+  const formData = new FormData(e.target);
+
+  try {
+    // Invia una richiesta POST al server con i dati del form
+    const res = await fetch('/upload', {
+      method: 'POST',
+      body: formData
+    });
+
+    // Estrae il testo dalla risposta
+    const text = await res.text();
+
+    // Stampa il messaggio del server nella console
+    console.log(text);
+
+  } catch (err) {
+    // Mostra un eventuale errore in console
+    console.error('Errore nel caricamento:', err);
+  }
+});
+
         break;
     }
   }
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-const path = require('path');
-const express = require('express');
-const multer = require('multer');
+// CONFIGURAZIONE SERVER ---------------------------------------------------|
+const express = require('express');    // Framework per creare server web
+const multer = require('multer');      // Middleware per gestire il caricamento di file
+const path = require('path');          // Utility per lavorare con percorsi di file e directory
+const fs = require('fs');             // File System per lavorare con file e directory 
 
-const app = express();
-const PORT = 3000;
+const app = express();                 // Crea un'app Express
+const PORT = 3000;             // Porta su cui il server ascolta
+
+app.use('/jquery', express.static('node_modules/jquery/dist'));
 
 // per inviare richieste con body json
 app.use(express.json());
 
-// definisco la root dei file statici
-app.use(express.static('public')); // HTML/CSS/JS personalizzati
-// bootstrap
+// direttiva file statici
+app.use(express.static('public'));
+// direttiva bootstrap
 app.use('/bootstrap', express.static('node_modules/bootstrap/dist')); 
 
+
+// MULTER CARICAMENTO FILE ----------------------------------------------------|
 // salvataggio file
 const storage = multer.diskStorage({
   // Definisco la cartella dove salvare i file caricati
@@ -20,37 +26,86 @@ const storage = multer.diskStorage({
     // cb(null, 'upload/') nessun errore (null) e salva in 'upload/'
     cb(null, 'upload/');
   },
-  // Funzione che definisce il nome con cui salvare il file
+
+  // Nome di salvataggio file
   filename: function (req, file, cb) {
     // cb(null, file.originalname) significa: nessun errore e usa il nome originale del file
     cb(null, file.originalname);
   }
 });
 
-// Creo l'oggetto multer con la configurazione storage sopra
+// Creo l'oggetto multer con la configurazione storage 
 const upload = multer({ storage: storage });
 
+// END MULTER -----------------------------------------------------------------|
+
+
 ////////////////////////////////////
-//      *****  ****  **** *****   //
-//      *    * *  * *       *     //
-//      *****  *  *  ***    *     //  
-//      *      *  *     *   *     //
-//      *      ****  ****   *     //
+///           REQUEST            ///  
 ////////////////////////////////////
-// Route POST per ricevere il file caricato
+
+// Route POST per ricevere i file caricati
 app.post('/upload', upload.single('file'), (req, res) => {
   // req.file contiene tutte le info del file caricato e salvato
-  // console.log(req.file);
+  console.log(req.file);
 
   // Rispondo al client che il file è stato salvato
   res.send('File salvato sul server!');
 });
 
+////////////////////////////////////
+///           DASHBOARD          ///  
+////////////////////////////////////
 
-// endpoint GET
-app.get('/public/assets', (req, res) => {
-  res.send('Server attivo!');
+
+
+
+// --------------------------
+// endpoint GET con query string
+app.get('/doc', (req, res) => {
+  if(req.query.nome)
+    res.type('text/plain').send('Hello '+req.query.nome); 
+  else{
+    res.status(400);
+    res.send('Nome non fornito');
+  }
 });
+
+// endpoint GET con parametri :name è il placeholder del parametro
+app.get('/doc/:nome', (req, res) => {
+  if(req.params.nome)
+    res.type('text/plain').send('Hello '+req.params.nome); 
+  else{
+    res.status(400);
+    res.send('Nome non fornito');
+  }
+
+});
+
+app.post('/doc/body/', (req, res) => {
+  console.log("POST route body");
+  res.type('text/plain').send('Hello '+req.body.nome);  
+});
+// ---------------------
+
+
+// Risposta alla chiamata GET asincrona per i valori medi
+app.get('/api/values_avg', (req, res) => {
+  fs.readFile('./upload/database.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).json({ error: 'Errore nel leggere il file' });
+      return;
+    }
+    const valori = JSON.parse(data);
+    res.json(valori);
+  });
+});
+
+
+////////////////////////////////////
+//            ANALISI             //  
+////////////////////////////////////
+
 
 
 app.listen(3000, () => {
