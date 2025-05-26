7cc970489df991c2fd2c1d6e251d4d024828c270
samuele <s.caporale1@campus.uniurb.it>
2025-05-26 15:25:38 +0200

    v0.0.7 GUI - aggiornato tutti i container con container-general

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,5 +1,5 @@
 // Versione GUI --------------------------------------------------------------|
-const version = "v0.0.6";
+const version = "v0.0.7";
 const versionElements = document.querySelectorAll(".version-number");
 
 for (const el of versionElements) {
