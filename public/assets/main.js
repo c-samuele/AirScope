b3fbaac03e6c968b5b1c30257b150a8d51db439b
samuele <s.caporale1@campus.uniurb.it>
2025-05-26 02:15:17 +0200

    server v.0.0.5 - sistemate dipendenze per generazione delle pagine services in directory dedicata

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,142 +1,103 @@
-// Versione GUI
+// Versione GUI --------------------------------------------------------------|
 const version = "v0.0.6";
 const versionElements = document.querySelectorAll(".version-number");
 
 for (const el of versionElements) {
   el.textContent = version;
 }
+//----------------------------------------------------------------------------|
 
-// Gestione del menu con link attivo
+// Gestione del menu con link attivo -----------------------------------------|
 document.querySelectorAll('#menu-nav .nav-link').forEach(link => {
-    link.addEventListener('click', function () {
-      document.querySelectorAll('#menu-nav .nav-link').forEach(item => {
-        item.classList.remove('active');
-      });
-      this.classList.add('active');
+  link.addEventListener('click', function () {
+    //  Rimuovo active da tutti i link
+    document.querySelectorAll('#menu-nav .nav-link').forEach(item => {
+      item.classList.remove('active');
     });
+    // Aggiungo active solo al link cliccato
+    this.classList.add('active');
   });
-// intercetto il click sul link del menu
+});
+// ---------------------------------------------------------------------------|
+
+// GENERAZIONE dei Servizi ------------------------------------------------------------------------------|
   document.getElementById("dashboard-btn").addEventListener("click", () => contentGenerate("dashboard"));
   document.getElementById("analitics-btn").addEventListener("click", () => contentGenerate("analitics"));
   document.getElementById("request-btn").addEventListener("click", () => contentGenerate("request"));
+// ------------------------------------------------------------------------------------------------------|
 
-// funzione per generare il contenuto della pagina servizio
- function contentGenerate(type) {
-    const container = $("#main-section");
 
-    switch(type) {
-      case "dashboard":
-        container.html(`
-<div class="container-fluid">
-<!-- TITOLO -->
-        <div class="col-12 mb-3">
-          <h1>Average air quality</h1>
-        </div>
-
-<!-- METRICHE -->
-    <div class="container-metrics">
-        <section class="d-flex flex-row item-card rounded-3">
-          <h3 class="title-metrics">CO</h3>
-          <div class="divider-metrics"></div>
-          <i class="metrics-value"><span id="val-co" class="data-avg"></span> <span class="metrics-udm">[mg/m³]</span></i>
-        </section>
-        
-        <section class="d-flex flex-row item-card rounded-3">
-          <h3 class="title-metrics">NO2</h3>
-          <div class="divider-metrics"></div>
-          <i class="metrics-value"><span id="val-no2" class="data-avg"></span> <span class="metrics-udm">[μg/m³]</span></i>
-        </section>
-      
-        <section class="d-flex flex-row item-card rounded-3">
-          <h3 class="title-metrics">NOx</h3>
-          <div class="divider-metrics"></div>
-          <i class="metrics-value"><span id="val-nox" style="color:red" class="data-avg"></span> <span class="metrics-udm">[μg/m³]</span></i>
-        </section>
-      
-        <section class="d-flex flex-row item-card rounded-3">
-          <h3 class="title-metrics">O3</h3>
-          <div class="divider-metrics"></div>
-          <i class="metrics-value"><span id="val-o3" class="data-avg"></span> <span class="metrics-udm">[μg/m³]</span></i>
-        </section>
-      
-        <section class="d-flex flex-row item-card rounded-3">
-          <h3 class="title-metrics">PM10</h3>
-          <div class="divider-metrics"></div>
-          <i class="metrics-value"><span id="val-pm10"  class="data-avg"></span> <span class="metrics-udm">[μg/m³]</span></i>
-        </section>
-    </div>
-</div>`);
-// Aggiorno i dati sul client dal server
-fetch('http://localhost:3000/api/values_avg')
-  .then(res => res.json())
-  .then(data => {
-    $('#val-co').text(data.CO);
-    $('#val-no2').text(data.NO2);
-    $('#val-nox').text(data.NOX);  
-    $('#val-o3').text(data.O3);
-    $('#val-pm10').text(data.PM10);
-  })
-  .catch(e => console.error(e));
-
-        break;
-      case "analitics":
-        container.html(`
-          <div class="container-fluid">
-            <p>Infographics,avg,table</p>
-            <canvas id="chart-database" class="rounded-3"></canvas>
-          </div>`);
-        break;
-      case "request":
-        container.html(`
-          <div class="container-fluid">
-<!-- form -->
-              <div class="container-form py-3">
-                <div class="col-sm-12 col-md-8 col-lg-6">
-                  <h2>POST</h2>
-                  <div class="form-api d-flex flex-column rounded-3 p-3">
-                      <form id="form-send" action="/upload" method="POST" enctype="multipart/form-data">
-                          <input class="form-control mb-3" type="file" name="file" required>
-                          <button class="btn btn-outline-primary px-3" type="submit">Carica</button>
-                      </form>
-                  </div>
-                </div>
-              </div>
-              
-
-<!-- end form -->
-          </div>`);
-  // Caricamento del file
-document.getElementById('form-send').addEventListener('submit', async (e) => {
-  e.preventDefault(); // Impedisce il ricaricamento della pagina
-
-  // Crea un oggetto FormData con i dati del form, incluso il file
-  const formData = new FormData(e.target);
-
-  try {
-    // Invia una richiesta POST al server con i dati del form
-    const res = await fetch('/upload', {
-      method: 'POST',
-      body: formData
+// funzione per generare il contenuto della pagina servizio ---------------------------------------------|
+function contentGenerate(type) {
+  const container = $("#main-section");
+  
+switch(type) {
+  case "dashboard":
+    container.load("services/dashboard.html", () => {
+      // Aggiorno i dati sul client dal server
+      fetch('http://localhost:3000/api/values_avg')
+        .then(res => res.json())
+        .then(data => {
+          $('#val-co').text(data.CO);
+          $('#val-no2').text(data.NO2);
+          $('#val-nox').text(data.NOX);
+          $('#val-o3').text(data.O3);
+          $('#val-pm10').text(data.PM10);
+        })
+        .catch(e => console.error(e));
     });
+  break;
 
-    // Estrae il testo dalla risposta
-    const text = await res.text();
+  case "analitics":
+    container.load("services/analytics.html", () => {
+      const ctx = $('#myChart');
+      new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+            label: '# Qualità Aria',
+            data: [5, 10, 22, 5, 21, 32],
+            borderWidth: 1
+            }]
+          },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            y: { beginAtZero: true }
+          }
+        }
+      });
+    });
+  break;
 
-    // Stampa il messaggio del server nella console
-    console.log(text);
+  case "request":
+     // Uso JQuery per caricare il contenuto
+    container.load("services/request.html", () => {
+      document.getElementById('form-send').addEventListener('submit', async (e) => {
+        // Blocco per gestirlo via AJAX
+        e.preventDefault();
+        // FormData contiene i dati del form
+        const formData = new FormData(e.target);
 
-  } catch (err) {
-    // Mostra un eventuale errore in console
-    console.error('Errore nel caricamento:', err);
-  }
-});
+        // SEZIONE CRITICA
+        try {
+          // RICHIESTA POST asincrona
+          const res = await fetch('/upload', {
+            method: 'POST',
+            body: formData
+          });
+          // LOG di Response
+          const text = await res.text();
+          console.log(text);
 
-        break;
+        // Catturo eventuali errori
+        } catch (err) {
+          console.error('Errore nel caricamento:', err);
+        }
+      });
+    });
+    break;
     }
   }
-
-
-
-
-
-
--- server.js
+++ server.js
@@ -2,94 +2,132 @@
 const express = require('express');    // Framework per creare server web
 const multer = require('multer');      // Middleware per gestire il caricamento di file
 const path = require('path');          // Utility per lavorare con percorsi di file e directory
-const fs = require('fs');             // File System per lavorare con file e directory 
+const fs = require('fs');              // File System per lavorare con file e directory 
+const csvParse = require('csv-parse'); // Per convertire csv in json
 
 const app = express();                 // Crea un'app Express
-const PORT = 3000;             // Porta su cui il server ascolta
+const PORT = 3000;                     // Porta su cui il server ascolta
 
+app.use(express.static('public'));     // direttiva file statici
+
+// Framework ----------------------------------------------------------------|
 app.use('/jquery', express.static('node_modules/jquery/dist'));
+app.use('/chart', express.static('node_modules/chart.js/dist'));
+app.use('/bootstrap', express.static('node_modules/bootstrap/dist')); 
 
 // per inviare richieste con body json
 app.use(express.json());
-
-// direttiva file statici
-app.use(express.static('public'));
-// direttiva bootstrap
-app.use('/bootstrap', express.static('node_modules/bootstrap/dist')); 
 
 
 // MULTER CARICAMENTO FILE ----------------------------------------------------|
 // salvataggio file
 const storage = multer.diskStorage({
-  // Definisco la cartella dove salvare i file caricati
   destination: function (req, file, cb) {
-    // cb(null, 'upload/') nessun errore (null) e salva in 'upload/'
     cb(null, 'upload/');
   },
-
-  // Nome di salvataggio file
   filename: function (req, file, cb) {
-    // cb(null, file.originalname) significa: nessun errore e usa il nome originale del file
     cb(null, file.originalname);
   }
 });
 
-// Creo l'oggetto multer con la configurazione storage 
 const upload = multer({ storage: storage });
-
 // END MULTER -----------------------------------------------------------------|
 
 
-////////////////////////////////////
-///           REQUEST            ///  
-////////////////////////////////////
-
+// REQUEST --------------------------------------------------------------------|
 // Route POST per ricevere i file caricati
 app.post('/upload', upload.single('file'), (req, res) => {
-  // req.file contiene tutte le info del file caricato e salvato
-  console.log(req.file);
 
-  // Rispondo al client che il file è stato salvato
-  res.send('File salvato sul server!');
-});
-
-////////////////////////////////////
-///           DASHBOARD          ///  
-////////////////////////////////////
-
-
-
-
-// --------------------------
-// endpoint GET con query string
-app.get('/doc', (req, res) => {
-  if(req.query.nome)
-    res.type('text/plain').send('Hello '+req.query.nome); 
-  else{
-    res.status(400);
-    res.send('Nome non fornito');
+  // LOG DI DEBUG --------------------|
+  // console.log('req.body:', req.body);
+  // console.log('req.file:', req.file);
+  // ---------------------------------|
+  if (!req.file) {
+    return res.status(400).send('Nessun file caricato!');
   }
+  // dati da analizzare
+  const citta = req.body.citta;
+  const ente = req.body.ente;
+  const csvFilePath = req.file.path;
+
+  // LOG DI DEBUG --------------------|
+  // console.log('Città:', citta);
+  // console.log('Ente:', ente);
+  // console.log('Percorso file:', csvFilePath);
+  // ---------------------------------|
+
+
+  const records = [];
+  fs.createReadStream(csvFilePath)
+    // columns: true <- prima riga legenda.
+    .pipe(csvParse.parse({ columns: true, trim: true }))
+    .on('data', function(row) {
+      records.push(row);
+    })
+    .on('end', function() {
+      console.log('CSV trasformato in oggetti:');
+      console.log(records.slice(0, 2));
+
+      const csvFileName = req.file.filename;
+      const jsonFileName = csvFileName.replace(/\.csv$/i, '.json');
+
+      const outputObject = {
+        citta: citta,
+        ente: ente,
+        data_caricamento: new Date().toISOString(),
+        fileJson: jsonFileName,
+        dati: records
+      };
+
+      const outputPath = path.join(__dirname, 'upload', jsonFileName);
+
+      fs.writeFile(outputPath, JSON.stringify(outputObject, null, 2), (err) => {
+        if (err) {
+          console.error('Errore nel salvataggio JSON:', err);
+          return res.status(500).send('Errore nel salvataggio del JSON.');
+        }
+        console.log('File JSON salvato:', outputPath);
+        res.send('File JSON creato correttamente!');
+      });
+    })
+    .on('error', function(err) {
+      console.error('Errore nel parsing:', err);
+      res.status(500).send('Errore nel parsing del CSV.');
+    });
 });
 
-// endpoint GET con parametri :name è il placeholder del parametro
-app.get('/doc/:nome', (req, res) => {
-  if(req.params.nome)
-    res.type('text/plain').send('Hello '+req.params.nome); 
-  else{
-    res.status(400);
-    res.send('Nome non fornito');
-  }
+// END REQUEST ------------------------------------------------------------------|
+    
+// esempi a lezione
+  // Endpoint GET con parametro URL (route param)
+  app.get('/doc/:nome', (req, res) => {
+    if (req.params.nome)
+      res.type('text/plain').send('Hello ' + req.params.nome);
+    else {
+      res.status(400);
+      res.send('Nome non fornito');
+    }
+  });
 
-});
+  // Endpoint GET con query parameter
+  app.get('/doc', (req, res) => {
+    if (req.query.nome)
+      res.type('text/plain').send('Hello ' + req.query.nome);
+    else {
+      res.status(400);
+      res.send('Nome non fornito');
+    }
+  });
 
-app.post('/doc/body/', (req, res) => {
-  console.log("POST route body");
-  res.type('text/plain').send('Hello '+req.body.nome);  
-});
-// ---------------------
+  // Endpoint POST con parametro nel body (richiesta JSON o form data)
+  app.post('/doc/body/', (req, res) => {
+    console.log("POST route body");
+    res.type('text/plain').send('Hello ' + req.body.nome);
+  });
 
 
-// Risposta alla chiamata GET asincrona per i valori medi
+// DASHBOARD --------------------------------------------------------------------|
+// Avg Value
 app.get('/api/values_avg', (req, res) => {
   fs.readFile('./upload/database.json', 'utf8', (err, data) => {
     if (err) {
@@ -101,12 +139,7 @@ app.get('/api/values_avg', (req, res) => {
   });
 });
 
-
-////////////////////////////////////
-//            ANALISI             //  
-////////////////////////////////////
-
-
+// END DASHBOARD ----------------------------------------------------------------|
 
 app.listen(3000, () => {
   console.log(`Server in ascolto su http://localhost:${PORT}`);
